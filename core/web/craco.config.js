a96999d236f00d84daf65777ef9848456dff19c0
MagisterFelix <felix.vlad.14@gmail.com>
2023-02-27 22:55:03 +0200

    Update configs

--- core/web/craco.config.js
+++ core/web/craco.config.js
@@ -5,7 +5,9 @@ const resolvePath = (p) => path.resolve(__dirname, p);
 module.exports = {
     webpack: {
         alias: {
-            '@styles': resolvePath('src/res/styles')
-        }
-    }
+            '@styles': resolvePath('src/res/styles'),
+            '@api': resolvePath('src/api'),
+            '@components': resolvePath('src/components')
+        },
+    },
 };
