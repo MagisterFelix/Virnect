dd17004a7fe67f928db715dc48697506e1983fc5
MagisterFelix <felix.vlad.14@gmail.com>
2023-03-01 20:13:30 +0200

    Automate getting links that require authorization

--- core/server/urls.py
+++ core/server/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from core.server.views.auth import AuthorizationView, RegistrationView
 
 urlpatterns = [
-    path("sign-in/", AuthorizationView().as_view(), name="sign_in"),
-    path("sign-up/", RegistrationView().as_view(), name="sign_up"),
+    path("sign-in/", AuthorizationView().as_view(), name="sign-in"),
+    path("sign-up/", RegistrationView().as_view(), name="sign-up"),
 ]
