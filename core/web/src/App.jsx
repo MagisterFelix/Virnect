e136437f086d811bc533c5fff5275e1a64b7aa63
MagisterFelix <felix.vlad.14@gmail.com>
2023-03-04 06:15:15 +0200

    Add password reset

--- core/web/src/App.jsx
+++ core/web/src/App.jsx
@@ -11,6 +11,7 @@ import '@fontsource/roboto/900.css';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import Authorization from '@components/auth/Authorization';
+import PasswordReset from '@components/auth/PasswordReset';
 import Registration from '@components/auth/Registration';
 import Home from '@components/Home';
 
@@ -42,6 +43,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={!isAuth ? <Authorization /> : <Navigate to="/" />} />
         <Route path="/sign-up" element={!isAuth ? <Registration /> : <Navigate to="/" />} />
+        <Route path="/reset-password/:uid?/:token?" element={!isAuth ? <PasswordReset /> : <Navigate to="/" />} />
       </Routes>
     </ThemeProvider>
   );
--- core/web/src/api/endpoints.jsx
+++ core/web/src/api/endpoints.jsx
@@ -1,6 +1,7 @@
 const ENDPOINTS = {
   authorization: '/api/sign-in/',
   registration: '/api/sign-up/',
+  reset_password: '/api/reset-password/',
 };
 
 export default ENDPOINTS;
