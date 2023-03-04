1512eade327322ff738a4cc00f575fdca659cde0
MagisterFelix <felix.vlad.14@gmail.com>
2023-03-04 21:54:31 +0200

    Change exception if user not found

--- core/server/serializers.py
+++ core/server/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.db.models import Q
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-from rest_framework import serializers
+from rest_framework import exceptions, serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from .models import User
@@ -16,7 +16,7 @@ class AuthorizationSerializer(TokenObtainPairSerializer):
         user = User.objects.get_or_none(Q(username=attr) | Q(email=attr))
 
         if user is None:
-            raise serializers.ValidationError("No user was found with these credentials.")
+            raise exceptions.AuthenticationFailed("No user was found with these credentials.")
 
         attrs["username"] = user.username
 
@@ -48,7 +48,7 @@ class PasswordResetSerializer(serializers.Serializer):
         user = User.objects.get_or_none(email=email)
 
         if user is None:
-            raise serializers.ValidationError("No user was found with this email.")
+            raise exceptions.AuthenticationFailed("No user was found with this email.")
 
         validated_data = {
             "email": email,
@@ -73,12 +73,12 @@ class PasswordResetConfirmSerializer(serializers.Serializer):
         try:
             pk = urlsafe_base64_decode(uid).decode()
         except (UnicodeDecodeError, ValueError):
-            raise serializers.ValidationError("Invalid password reset link.")
+            raise exceptions.ValidationError("Invalid password reset link.")
 
         user = User.objects.get_or_none(pk=pk)
 
         if not default_token_generator.check_token(user, token):
-            raise serializers.ValidationError("Invalid password reset link.")
+            raise exceptions.ValidationError("Invalid password reset link.")
 
         user.set_password(password)
         user.save()
