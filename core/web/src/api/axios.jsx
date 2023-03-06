146db08a392dbeede48786e3b8203fd6e17dd9ed
MagisterFelix <felix.vlad.14@gmail.com>
2023-03-06 23:03:32 +0200

    Fix image processing

--- core/web/src/api/axios.jsx
+++ core/web/src/api/axios.jsx
@@ -3,8 +3,10 @@ import { makeUseAxios } from 'axios-hooks';
 
 import Cookies from 'js-cookie';
 
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000' : '';
+
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000' : '',
+  baseURL,
   withCredentials: true,
 });
 
@@ -15,6 +17,22 @@ instance.interceptors.request.use(
       request.headers['X-CSRFToken'] = csrf;
     }
     return request;
+  },
+  (error) => Promise.reject(error),
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    if (response.data.avatar) {
+      response.data.avatar = `${baseURL}${response.data.avatar}`;
+    }
+    if (response.data.image) {
+      response.data.image = `${baseURL}${response.data.image}`;
+    }
+    if (response.data.icon) {
+      response.data.icon = `${baseURL}${response.data.icon}`;
+    }
+    return response;
   },
   (error) => Promise.reject(error),
 );
