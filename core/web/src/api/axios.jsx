dae75daa8931365864ff36fc23a893ff3407129c
MagisterFelix <felix.vlad.14@gmail.com>
2023-03-13 03:52:53 +0200

    Add content-type to axios instance

--- core/web/src/api/axios.jsx
+++ core/web/src/api/axios.jsx
@@ -7,6 +7,9 @@ const baseURL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000'
 
 const instance = axios.create({
   baseURL,
+  headers: {
+    'Content-Type': 'multipart/form-data',
+  },
   withCredentials: true,
 });
 
