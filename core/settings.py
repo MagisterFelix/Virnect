21510d3e9170e968943d8ace196e2b8b07f5ebf5
MagisterFelix <felix.vlad.14@gmail.com>
2023-04-02 21:36:06 +0300

    Add django-filter

--- core/settings.py
+++ core/settings.py
@@ -26,8 +26,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
     "corsheaders",
+    "rest_framework",
+    "django_filters",
     "core.server",
 ]
 
@@ -131,6 +132,9 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
+    ],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
     ],
 }
 
