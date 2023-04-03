34fa9555ef22635ae7d0003f5190800d0755d3a0
MagisterFelix <felix.vlad.14@gmail.com>
2023-04-03 18:50:02 +0300

    Add ability to get unique tags by name

--- core/server/views/tag.py
+++ core/server/views/tag.py
@@ -10,3 +10,20 @@ class TagListView(ListCreateAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagListSerializer
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = super(TagListView, self).get_queryset()
+
+        unique = self.request.query_params.get("unique")
+        if unique is not None:
+            ids = set()
+            names = set()
+
+            for tag in queryset:
+                if tag.name not in names:
+                    ids.add(tag.id)
+                    names.add(tag.name)
+
+            queryset = queryset.filter(id__in=ids)
+
+        return queryset
