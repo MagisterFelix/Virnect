23f2cd88cea7162e2eb3a61902a7eef812a32177
MagisterFelix <felix.vlad.14@gmail.com>
2023-04-09 02:07:00 +0300

    Remove strict mode

--- core/web/src/index.jsx
+++ core/web/src/index.jsx
@@ -6,4 +6,4 @@ import App from './App';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(process.env.NODE_ENV === 'development' ? <React.StrictMode><App /></React.StrictMode> : <App />);
+root.render(<App />);
